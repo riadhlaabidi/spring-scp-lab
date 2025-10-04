6a982481be8675e382dcc18ce8466325b81a9345
riadhlaabidi <riadh.by.laabidi@gmail.com>
2025-10-04 15:59:43 +0100

    JDBC spring boot lab

---
+++ 30-jdbc-boot/src/main/java/rewards/JdbcBootApplication.java
@@ -0,0 +1,24 @@
+package rewards;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+@SpringBootApplication
+public class JdbcBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbcBootApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner commandLineRunner(JdbcTemplate jdbcTemplate) {
+		String query = "SELECT count(*) FROM T_ACCOUNT";
+
+		return args -> System.out.println("Hello, there are "
+				+ jdbcTemplate.queryForObject(query, Long.class)
+				+ " accounts");
+	}
+}
